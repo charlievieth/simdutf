37e7ae73125b8785646438bbe9ddee1483455be7
Charlie Vieth <charlie.vieth@gmail.com>
2025-06-02 23:16:49 -0400

    gh: add update simdutf script/action

--- scripts/update-simdutf.py
+++ scripts/update-simdutf.py
@@ -1,38 +1,40 @@
 #!/usr/bin/env python3
 
 import json
+import logging
 import os.path
-import pprint
 import re
 import shutil
 import subprocess
 import sys
 import tempfile
+import urllib.parse
 import urllib.request
 import zipfile
 from functools import cache
 from typing import Any
+from typing import Callable
 
 SEMVER_RE = r"^v\d+\.\d+\.\d+$"
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-# class Semver:
-#     major = 0
-#     minor = 0
-#     patch = 0
-#
-#     def __init__(self, major: int, minor: int, patch: int):
-#         self.major = major
-#         self.minor = minor
-#         self.patch = patch
+
+def get_logger(level: int = logging.INFO) -> logging.Logger:
+    logging.basicConfig(
+        level=level,
+        format="[%(levelname)s] [%(filename)s:%(lineno)d]: %(message)s",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+    return logging.getLogger("update-simdutf")
 
 
-def perror(*values: object) -> None:
-    print(file=sys.stderr, *values)
+# Global logger
+log = get_logger()
 
 
 def current_simdutf_version() -> str:
+    """Return the version of the bundled simdutf library."""
     with open(os.path.join(PROJECT_ROOT, "SIMDUTF_VERSION"), encoding="utf-8") as f:
         return f.read().strip()
 
@@ -52,17 +54,24 @@ def latest_simdutf_release() -> dict[str, Any]:
         return json.load(f)
 
 
-def append_go_build_tag(filename: str) -> None:
+def modify_file(filename: str, fn: Callable[[str], str]) -> None:
     with open(filename, mode="r+t", encoding="utf-8") as f:
         src = f.read()
         f.seek(0, 0)
         f.truncate(0)
-        f.write("//go:build !libsimdutf\n\n")
-        f.write(src)
+        f.write(fn(src))
 
 
-# TODO: rename to indicate that we also update the files
-def download_release(version: str) -> None:
+def append_go_build_tag(filename: str) -> None:
+    def fn(src: str) -> str:
+        if "//go:build !libsimdutf" not in src:
+            src = "//go:build !libsimdutf\n\n" + src
+        return src
+
+    modify_file(filename, fn)
+
+
+def update_release(version: str) -> None:
     req = urllib.request.Request(
         url=f"https://github.com/simdutf/simdutf/releases/download/{version}/singleheader.zip",
         method="GET",
@@ -85,13 +94,30 @@ def download_release(version: str) -> None:
                 dst=os.path.join(PROJECT_ROOT, name),
             )
 
-    version_file = os.path.join(PROJECT_ROOT, "SIMDUTF_VERSION")
-    with open(version_file, mode="w+", encoding="utf-8") as f:
-        f.write(f"{version}\n")
+    def update_readme(src: str) -> str:
+        # The earliest supported version of simdutf is v7.x.x
+        for m in re.findall(r"(v[7-9]\d*\.\d+\.\d+)", src):
+            src = src.replace(m, version)
+        return src
+
+    modify_file(
+        os.path.join(PROJECT_ROOT, "README.md"),
+        update_readme,
+        # lambda src: src.replace(current_simdutf_version(), version),
+    )
+    modify_file(
+        os.path.join(PROJECT_ROOT, "SIMDUTF_VERSION"),
+        lambda _: f"{version}\n",
+    )
 
 
-def test_new_release() -> None:
-    subprocess.check_output(["go", "test", "-race", "./..."], cwd=PROJECT_ROOT)
+def run_go_tests() -> None:
+    subprocess.check_call(
+        ["go", "test", "-race", "./..."],
+        cwd=PROJECT_ROOT,
+        stdout=sys.stdout,
+        stderr=sys.stderr,
+    )
 
 
 def latest_simdutf_version() -> str:
@@ -101,32 +127,211 @@ def latest_simdutf_version() -> str:
     return ver
 
 
-def get_singleheader_download_url(rel: dict[str, Any]) -> str:
-    for a in rel["assets"]:
-        if a["name"] == "singleheader.zip":
-            return a["browser_download_url"]
-    raise Exception("singleheader not found in release")
+def git(*args) -> str:
+    return subprocess.check_output(
+        ["git"] + list(*args),
+        cwd=PROJECT_ROOT,
+        stderr=subprocess.STDOUT,
+        encoding="utf-8",
+    )
 
 
-def update_simdutf(target: str) -> None:
-    rel = latest_simdutf_release()
-    if rel["tag_name"] != target:
-        raise Exception(f"latest simdutf release {rel["tag_name"]} != {target}")
-    url = get_singleheader_download_url(rel)
+def pr_exists(branch_name: str) -> bool:
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    query = urllib.parse.urlencode({"head": f"charlievieth:{branch_name}"})
+    req = urllib.request.Request(
+        url=f"https://api.github.com/repos/charlievieth/simdutf/pulls?{query}",
+        headers=headers,
+        method="GET",
+    )
+    with urllib.request.urlopen(req, timeout=5) as f:
+        return len(json.load(f)) == 0
+
+
+def modified_files() -> set[str]:
+    pattern = re.compile(r"^\s*M\s+(.*)")
+    modified = []
+    for line in git("status", "--porcelain").splitlines():
+        if len(m := pattern.findall(line)):
+            modified.append(m[0])
+    return set(modified)
+
+
+def branch_name(version: str) -> str:
+    return f"deps/{version}"
+
+
+def create_pr(old_version: str, new_version: str, auth_token: str) -> None:
+    branch = branch_name(new_version)
+    if pr_exists(branch):
+        log.warn("PR already exists for version %s: nothing to do", new_version)
+        return
+
+    if not modified_files():
+        raise RuntimeError("no files were modified")
+
+    git("checkout", "-b", branch)
+    git("add", "--update", "simdutf.h", "simdutf.cpp", "SIMDUTF_VERSION", "README.md")
+
+    title = f"deps: update bundled library version from {old_version} to {new_version}"
+    git(
+        "commit",
+        "-m",
+        title,
+        "-m",
+        "Update the bundled simdutf library from version {old_version} to {new_version}.",
+    )
+
+    def rel_url(version: str) -> str:
+        return f"[{version}](https://github.com/simdutf/simdutf/releases/tag/{version})"
+
+    body = {
+        "title": title,
+        # "body": "This commit updates the bundled simdutf library from version "
+        # + f"{rel_url(old_version)} to {rel_url(new_version)}.",
+        "body": "This commit updates the bundled simdutf library from version %s to %s".format(
+            {rel_url(old_version)}, {rel_url(new_version)}
+        ),
+        "head": branch,
+        "base": "master",
+    }
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": auth_token,
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    req = urllib.request.Request(
+        url="https://api.github.com/repos/charlievieth/simdutf/releases/latest",
+        headers=headers,
+        data=json.dumps(body).encode("utf-8"),
+        method="POST",
+    )
+    with urllib.request.urlopen(req, timeout=5) as f:
+        res = json.load(f)
+    log.info("-- created pr:")
+    json.dump(sys.stdout, res, indent=4)
+    log.info("--")
 
 
 def main() -> int:
-    if current_simdutf_version() == latest_simdutf_version():
+    current_version = current_simdutf_version()
+    latest_version = latest_simdutf_version()
+    if current_version == latest_version:
+        log.info(
+            "nothing to do here: simdutf version %s is the latest: exiting",
+            current_version,
+        )
         return 0
 
     rel = latest_simdutf_release()
     if rel["draft"] or rel["prerelease"]:
-        perror(f"Refusing to update to draft/pre-release version: {rel["tag_name"]}")
+        log.warn(
+            "refusing to update to draft/pre-release version: %s",
+            rel["tag_name"],
+        )
         return 1
 
-    download_release(rel["tag_name"])
+    update_release(latest_version)
+    run_go_tests()
+    if not os.environ["SIMDUTF_GH_ACTIONS"]:
+        log.fatal("missing SIMDUTF_GH_ACTIONS token - cannot create PR")
+        return 1
+    create_pr(
+        old_version=current_version,
+        new_version=latest_version,
+        auth_token=os.environ["SIMDUTF_GH_ACTIONS"],
+    )
+
     return 0
 
 
 if __name__ == "__main__":
     sys.exit(main())
+
+
+# class Semver:
+#     __slots__ = ("major", "minor", "patch", "tag")
+#
+#     major = 0
+#     minor = 0
+#     patch = 0
+#     tag = ""
+#
+#     def __init__(self, major: int, minor: int, patch: int, tag: str = ""):
+#         self.major = major
+#         self.minor = minor
+#         self.patch = patch
+#         self.tag = tag
+#
+#     # def __lt__(self, value: object, /) -> bool:
+#     #     pass
+#
+#     def __eq__(self, other: object) -> bool:
+#         if not isinstance(other, Semver):
+#             return NotImplemented
+#         return bool(
+#             self.major == other.major
+#             and self.minor == other.minor
+#             and self.patch == other.patch
+#             and self.tag == other.tag
+#         )
+#
+#     def __ne__(self, other: object) -> bool:
+#         if not isinstance(other, Semver):
+#             return NotImplemented
+#         return not self.__eq__(other)
+#
+#     def __lt__(self, other: object) -> bool:
+#         if not isinstance(other, Semver):
+#             return NotImplemented
+#         return bool(
+#             self.major < other.major
+#             or self.minor < other.minor
+#             or self.patch < other.patch
+#             or self.tag < other.tag
+#         )
+#
+#     def __gt__(self, other: object) -> bool:
+#         if not isinstance(other, Semver):
+#             return NotImplemented
+#         return bool(
+#             self.major > other.major
+#             or self.minor > other.minor
+#             or self.patch > other.patch
+#             or self.tag > other.tag
+#         )
+#
+#
+#     @classmethod
+#     def parse(cls, ver: str) -> "Semver":
+#         if ver.startswith("v"):
+#             ver = ver[1:]
+#         a = ver.split(".")
+#         if len(a) == 3:
+#             tag = ""
+#             if "-" in a[-1]:
+#                 tag = a[-1][a[-1].index("-") + 1:]
+#             else:
+#                 tag = ""
+#             return Semver(int(a[0]), int(a[1]), int(a[2]), tag=tag)
+#         return Semver(0, 0, 0)
+
+
+# # WARN: use this
+# def get_singleheader_download_url(rel: dict[str, Any]) -> str:
+#     for a in rel["assets"]:
+#         if a["name"] == "singleheader.zip":
+#             return a["browser_download_url"]
+#     raise Exception("singleheader not found in release")
+#
+#
+# # WARN: use this
+# def update_simdutf(target: str) -> None:
+#     rel = latest_simdutf_release()
+#     if rel["tag_name"] != target:
+#         raise Exception(f"latest simdutf release {rel["tag_name"]} != {target}")
+#     # WARN: why is this not used
+#     url = get_singleheader_download_url(rel)
